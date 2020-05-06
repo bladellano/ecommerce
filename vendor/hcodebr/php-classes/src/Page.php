d53d4629c33e272c967ebf5d4170ec9fc28630f4
Caio Silva <bladellano@gmail.com>
2020-05-05 22:39:17 -0300

    Tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -10,24 +10,25 @@ class Page
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data" => [],
     ];
-    private $folder = "/projeto-ecommerce/";
 
     public function __construct($opts = array(),$tpl_dir="/views/")
     {
         
         $this->options = array_merge($this->defaults, $opts);
         $config = array(
-            "tpl_dir" => $_SERVER["DOCUMENT_ROOT"] . $this->folder. $tpl_dir,
-            "cache_dir" => $_SERVER["DOCUMENT_ROOT"] . $this->folder. "/views-cache/",
+            "tpl_dir" => $_SERVER["DOCUMENT_ROOT"] . $tpl_dir,
+            "cache_dir" => $_SERVER["DOCUMENT_ROOT"] . "/views-cache/",
             "debug" => false,
         );
 
         Tpl::configure($config);
         $this->tpl = new Tpl;
         $this->setData($this->options["data"]);       
-        $this->tpl->draw("header");
+         if($this->options["header"]===true) $this->tpl->draw("header");
 
     }
 
@@ -44,7 +45,7 @@ class Page
 
     public function __destruct()
     {
-        $this->tpl->draw("footer");
+        if($this->options["header"]===true) $this->tpl->draw("footer");
     }
 
 }
