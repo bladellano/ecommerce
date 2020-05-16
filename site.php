da7fd0b69d8d6f08b7de6c1ec0146b2db14acbd2
Caio Silva <bladellano@gmail.com>
2020-05-15 22:34:38 -0300

    Categorias Vs Produtos

--- site.php
+++ site.php
@@ -24,7 +24,7 @@ $app->get("/categories/:idcategory",function($idcategory){
 
     $page->setTpl("category",[
         'category'=>$category->getValues(),
-        'products'=>[]
+        'products'=>Products::checkList($category->getProducts())
     ]);
 
 });
--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -4,6 +4,7 @@ namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model;
+use \Hcode\Model\Products;
 
 class Category extends Model
 {
@@ -36,8 +37,8 @@ class Category extends Model
     {
         $sql = new Sql();
 
-        $results = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory",array(
-            ":idcategory"=>$idcategory
+        $results = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory", array(
+            ":idcategory" => $idcategory,
         ));
         $this->setData($results[0]);
     }
@@ -46,8 +47,8 @@ class Category extends Model
     {
         $sql = new Sql();
 
-        $sql->query('DELETE FROM tb_categories WHERE idcategory = :idcategory',array(
-            ":idcategory"=>$this->getidcategory()
+        $sql->query('DELETE FROM tb_categories WHERE idcategory = :idcategory', array(
+            ":idcategory" => $this->getidcategory(),
         ));
 
         Category::updateFile();
@@ -59,12 +60,55 @@ class Category extends Model
 
         $html = [];
 
-        foreach($categories as $row){
-            array_push($html,'<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+        foreach ($categories as $row) {
+            array_push($html, '<li><a href="/categories/' . $row['idcategory'] . '">' . $row['descategory'] . '</a></li>');
         }
 
-        file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR."categories-menu.html",implode('',$html));
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
     }
 
+    public function getProducts($related = true)
+    {
+        $sql = new Sql();
+
+        if ($related === true) {
+            return $sql->select("SELECT * FROM tb_products
+                WHERE idproduct IN(SELECT a.idproduct
+                FROM tb_products a
+                INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+                WHERE b.idcategory = :idcategory)", array(
+                ":idcategory" => $this->getidcategory(),
+            ));
+
+        } else {
+            return $sql->select("SELECT * FROM tb_products
+                WHERE idproduct NOT IN(SELECT a.idproduct
+                FROM tb_products a
+                INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+                WHERE b.idcategory = :idcategory)", array(
+                ":idcategory" => $this->getidcategory(),
+            ));
+        }
+    }
+
+    public function addProduct(Products $product)
+    {
+         $sql = new Sql();
+
+         $sql->query("INSERT INTO tb_productscategories (idcategory,idproduct) VALUES (:idcategory, :idproduct)",[
+             ":idcategory" => $this->getidcategory(),
+             ":idproduct" => $product->getidproduct()
+         ]);
+    }
+
+
+    public function removeProduct(Products $product)
+    {
+         $sql = new Sql();
+         $sql->query("DELETE FROM tb_productscategories WHERE idcategory = :idcategory AND idproduct = :idproduct",[
+             ":idcategory" => $this->getidcategory(),
+             ":idproduct" => $product->getidproduct()
+         ]);
+    }
 
 }
