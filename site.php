b3486f045e4decc610976c73668ba46c8ebe684e
Caio Silva <bladellano@gmail.com>
2020-06-05 11:46:39 -0300

    Site Cadastro

--- site.php
+++ site.php
@@ -130,13 +130,14 @@
         $page = new Page();
 
         $page->setTpl('login', [
-            'error' => User::getError()
+            'error' => User::getError(),
+            'errorRegister' => User::getErrorRegister(),
+            'registerValues' => (isset($_SESSION['registerValues'])) ? $_SESSION['registerValues'] : ['name' => '', 'email' => '', 'phone' => '']
         ]);
     });
 
 
     $app->post('/login', function () {
-
         try {
             User::login($_POST['login'], $_POST['password']);
         } catch (\Exception $e) {
@@ -146,9 +147,54 @@
         exit;
     });
 
-
     $app->get('/logout', function () {
         User::logout();
         header('Location:/login');
+        exit;
+    });
+
+    $app->post('/register', function () {
+
+        $_SESSION['registerValues'] = $_POST;
+
+        if (!isset($_POST['name']) || $_POST['name'] == '') {
+            User::setErrorRegister('Preencha seu nome.');
+            header("Location: /login");
+            exit;
+        }
+
+        if (!isset($_POST['email']) || $_POST['email'] == '') {
+            User::setErrorRegister('Preencha seu e-mail.');
+            header("Location: /login");
+            exit;
+        }
+
+        if (!isset($_POST['password']) || $_POST['password'] == '') {
+            User::setErrorRegister('Preencha sua senha.');
+            header("Location: /login");
+            exit;
+        }
+
+        if (User::checkLoginExist($_POST['email']) == true) {
+            User::setErrorRegister('Este endereço de e-mail já está sendo usado por outro usuário.');
+            header('Location: /login');
+            exit;
+        }
+        $user = new User();
+
+        $user->setData([
+            'inadmin' => 0,
+            'deslogin' => $_POST['email'],
+            'desperson' => $_POST['name'],
+            'desemail' => $_POST['email'],
+            'despassword' => $_POST['password'],
+            'nrphone' => (int) $_POST['phone']
+        ]);
+
+        $user->save();
+
+        User::login($_POST['email'], $_POST['password']); //Faz logo autenticação
+
+        header("Location: /checkout");
         exit;
     });
